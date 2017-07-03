73a9996d6da594a47ce1a3ae1b0e18d67126e685
foldik-learn <foldi.kristof.work@gmail.com>
2017-07-04 00:17:30 +0200

    Created command handler with collatz command

--- js/app.js
+++ js/app.js
@@ -1,14 +1,62 @@
 var app = app || {};
 
-$(document).ready(function() {
-    $("#line-1").focus();
-    $("#line-1").keypress(function(event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            console.log($("#line-1").text())
+app.collatz = function() {
+    var n = 101;
+    var sequence = [];
+    do {
+        sequence.push(n);
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
         }
-    })
-    $("#line-1").parent().click(function(event) {
-        $("#line-1").focus();
-    })
-})
+    } while (n > 1);
+    return sequence;
+}
+
+app.commandHandler = (function($document) {
+
+    var inputLineCount = 1;
+    var $actualInputLine;
+
+    function setUpCommandLine() {
+        $actualInputLine.focus();
+        $actualInputLine.keypress(function(event) {
+            if (event.which == 13) {
+                event.preventDefault();
+                evaluateCommand();
+            }
+        })
+        $actualInputLine.parent()
+            .click(function(event) {
+                $actualInputLine.focus();
+            });
+    }
+
+    function appendNextInputLine() {
+        $actualInputLine.attr('contentEditable', 'false');
+        inputLineCount = inputLineCount + 1;
+        $('#container').append('<div class="input-line-container"><span class="input-line-marker">&gt;</span><div id="line-' + inputLineCount + '" class="input-line" contentEditable="true"></div></div>');
+        $actualInputLine = $('#line-' + inputLineCount);
+        setUpCommandLine();
+    }
+
+    function evaluateCommand() {
+        if ($actualInputLine.text() === 'collatz') {
+            $('#container').append('<div class="result-container">[' + app.collatz().join(' | ') + ']</div>')
+        } else {
+            $('#container').append('<div class="result-container">[' + $actualInputLine.text() + ']</div>')
+        }
+        appendNextInputLine();
+    }
+
+    $document.ready(function() {
+        $actualInputLine = $('#line-1');
+        setUpCommandLine();
+    });
+
+    return {
+        evaluateCommand: evaluateCommand
+    }
+
+})($(document));

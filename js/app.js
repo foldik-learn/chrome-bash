c511eecd59549e45dca2d2e970fc832500dfa327
foldik-learn <foldi.kristof.work@gmail.com>
2017-07-03 22:19:10 +0200

    Created input line element

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 var app = app || {};
+
+$(document).ready(function() {
+    $("#line-1").focus();
+    $("#line-1").keypress(function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            console.log($("#line-1").text())
+        }
+    })
+    $("#line-1").parent().click(function(event) {
+        $("#line-1").focus();
+    })
+})
